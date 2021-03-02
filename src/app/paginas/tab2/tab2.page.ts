d320f7fa86da33a8029e0736e09e776ecbd3082d
Sergio Alvarado Gutierrez <palvarado@elfec.com>
2021-03-02 15:10:45 -0400

    infinite scroll categories

--- src/app/paginas/tab2/tab2.page.ts
+++ src/app/paginas/tab2/tab2.page.ts
@@ -12,24 +12,43 @@ export class Tab2Page  implements OnInit {
   @ViewChild(IonSegment, {static: true}) segmento : IonSegment;
   categories: any[] = ["business", "entertainment", "general", "health", "science", "sports", "technology"];
   categorySelected: String = "business";
-  articles: Article[];
+  articles: Article[] = [];
+  page_number = 1;
+  page_limit = 10;
   constructor(private newsService: NewsService) {}
 
   ngOnInit() {
-    this.segmento.value = this.categories[0];
-    let params = { category: this.categorySelected };
-    this.getArticles(params);
+    this.segmento.value = this.categories[0];    
+    this.categorySelected = this.segmento.value;
+    this.getArticles(null);
   }
 
   segmentChanged(e){     
     this.categorySelected = e.detail.value;
-   this.getArticles({ category: this.categorySelected });
+    this.articles = [];
+    this.page_number = 1;
+   this.getArticles(null);
   }
 
-  getArticles(params){
+  
+
+  getArticles(event?){
+    let params = { country: "us", category: this.categorySelected, page: this.page_number, pageSize: this.page_limit };
     this.newsService.getArticles(params).subscribe(res => {
-      this.articles = res.articles;
+      this.articles.push(...res.articles);
+      if (res.articles.length == 0){
+        event.target.disabled = true;
+        event.target.complete();
+        return;
+      }
+      if(event)
+        event.target.complete();
+      this.page_number++;
     })
+  }
+
+  doInfinite(event) {
+    this.getArticles(event);
   }
 
 }
